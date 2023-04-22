291d63b0ffd666e5817ff8fa0f60ce86fed2af4d
Aandi1989 <navaliny1989@gmail.com>
2023-04-22 20:03:16 +0300

    searchForm block media in process

--- src/script/index.js
+++ src/script/index.js
@@ -15,6 +15,18 @@ function removeClasses(elements, className){
         element.classList.remove(className);
     });
 }
+// ibg function
+function ibg(){
+    let ibg=document.querySelectorAll("._ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+    }
+    }
+    ibg();
+
+
 
 // /Functions.js
 
